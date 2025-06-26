16e08257a8da019242c8fdab9316b10d99aee820
Shubham Singh <136968270+LostBoy143@users.noreply.github.com>
2025-06-26 23:12:08 +0530

    Added a dit

--- rss-feed-content-creator-main/src/app/dashboard/page.js
+++ rss-feed-content-creator-main/src/app/dashboard/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="p-2 text-white border-gray-700 px-4 text-lg w-full">
       Welcome to My Content Creator Start surfing
       by selecting your favourite topic! Your
-      results will be shown here....
+      results will be shown here.....
     </div>
   );
 }
