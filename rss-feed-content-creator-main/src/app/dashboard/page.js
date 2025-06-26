7e03a9776c95e949e4aa18a0d39e322ca871fdf3
Shubham Singh <136968270+LostBoy143@users.noreply.github.com>
2025-06-26 23:09:25 +0530

    added a dot

--- rss-feed-content-creator-main/src/app/dashboard/page.js
+++ rss-feed-content-creator-main/src/app/dashboard/page.js
@@ -3,8 +3,9 @@
 export default function Home() {
   return (
     <div className="p-2 text-white border-gray-700 px-4 text-lg w-full">
-      Welcome to My Content Creator Start surfing by selecting your favourite
-      topic! Your results will be shown here...
+      Welcome to My Content Creator Start surfing
+      by selecting your favourite topic! Your
+      results will be shown here....
     </div>
   );
 }
